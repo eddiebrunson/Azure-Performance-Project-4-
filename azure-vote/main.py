28a8f8f561f5c5580f939b2836aadfa80665c24a
Edward Brunson <eddiebrunson@gmail.com>
2021-02-08 00:25:52 -0500

    Feat: Fixing logging

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 # App Insights
 # TODO: Import required libraries for App Insights
-from applicationinsights import TelemetryClient
+
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 from opencensus.ext.azure import metrics_exporter
 from opencensus.stats import aggregation as aggregation_module
@@ -85,14 +85,10 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
-        tracer.span("CatVotes")
-        tc.track_event("CatVotes")
-        tc.flush()
+        tracer.span(name="CatVotes")
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-        tracer.span("DogVotes")
-        tc.track_event("DogVotes")
-        tc.flush()
+        tracer.span(name="DogVotes")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
